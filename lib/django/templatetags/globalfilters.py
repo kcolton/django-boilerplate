34f8c5db89bb56c7868be87ad04904997e1fb0b6
Ken Colton <kcolton@gmail.com>
2013-07-17 19:59:18 -0400

    Added has_attr global template filter

--- lib/django/templatetags/globalfilters.py
+++ lib/django/templatetags/globalfilters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def static(path):
     return staticfiles_storage.url(path)
+
+@register.filter
+def has_attr(object, attr):
+    return hasattr(object, attr)
