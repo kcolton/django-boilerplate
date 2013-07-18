b6e90e56aec31591a223bf7673f65a682db1e7ff
Ken Colton <kcolton@gmail.com>
2013-07-18 16:44:44 -0400

    Added db backed sessions by default

--- config/local/settings.py
+++ config/local/settings.py
@@ -4,11 +4,11 @@ ENV = 'local'
 
 ASSETS_DEBUG = True
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': os.path.join(TOP_ROOT, 'db.sqlite'),                 # Or path to database file if using sqlite3.
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(TOP_ROOT, 'db.sqlite'),                 # Or path to database file if using sqlite3.
+    }
+}
 
-# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
