990989db814d2ba29160dc79baba9650ceeffb3f
Ken Colton <kcolton@gmail.com>
2013-07-17 20:30:50 -0400

    Added favicon

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,4 +11,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # Some globally annoying URLs that we don't want django spending much time on
+    # Please cache this shit out of this
+    url(r'^favicon.ico$', RedirectView.as_view(url=staticfiles_storage.url('app/favicon.ico'))),
 )
