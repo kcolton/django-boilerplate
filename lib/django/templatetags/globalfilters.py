82dfce4c41d9a4f6daffacfcd4d385e77fd112a0
Ken Colton <kcolton@gmail.com>
2013-10-26 00:18:08 -0400

    Added more handy template filters

--- lib/django/templatetags/globalfilters.py
+++ lib/django/templatetags/globalfilters.py
@@ -1,6 +1,8 @@
-from django import template
+from dateutil import parser
+from django import template, forms
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.utils.timezone import get_current_timezone
+from django.utils.timezone import get_current_timezone_name
+import pytz
 
 register = template.Library()
 
@@ -12,22 +14,57 @@ def static(path):
 
 @register.filter
 def localtime(dt):
-    return dt.astimezone(get_current_timezone())
+    try:
+        timezone = pytz.timezone(get_current_timezone_name())
+        return timezone.localize(dt) if dt.tzinfo is None else dt.astimezone(timezone)
+    except AttributeError:
+        return dt
+
+
+@register.filter
+def parsedatetime(dt_str):
+    if not dt_str:
+        return None
+
+    try:
+        return parser.parse(dt_str)
+    except ValueError:
+        return None
+
+
+@register.filter
+def split(to_split, *args):
+    return to_split.split(*args)
 
 
 @register.filter
 def is_checkbox(field):
-    return classname(field.field.widget) == 'CheckboxInput'
+    return isinstance(field.field.widget, forms.CheckboxInput)
 
 
 @register.filter
 def is_radio(field):
-    return classname(field.field.widget) == 'RadioSelect'
+    return isinstance(field.field.widget, forms.RadioSelect)
 
 
 @register.filter
 def is_checkbox_multiple(field):
-    return classname(field.field.widget) == 'CheckboxSelectMultiple'
+    return isinstance(field.field.widget, forms.CheckboxSelectMultiple)
+
+
+@register.filter
+def is_file(field):
+    return isinstance(field.field.widget, forms.FileInput)
+
+
+@register.filter
+def is_date(field):
+    return isinstance(field.field.widget, forms.DateInput)
+
+
+@register.filter
+def is_datetime(field):
+    return isinstance(field.field.widget, forms.DateTimeInput)
 
 
 @register.filter
