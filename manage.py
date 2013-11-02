e91a2e8c5524bbfc45f2b132bb25aaf257d0dbdb
Ken Colton <kcolton@gmail.com>
2013-11-02 19:20:46 -0400

    Allow passing of --app-config command line argument to manage.py

--- manage.py
+++ manage.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python
 import os
 import sys
-import imp
-import pprint
 from django.core.management import execute_from_command_line
+from lib import utils
 from lib.django.utils.sanity import check_sanity
 
 if __name__ != '__main__':
-    raise Exception('manage.py should be run directly and not be imported')
+    raise Exception("manage.py should be run directly and not be imported")
 
-APP_CONFIG = os.environ.get('APP_CONFIG', 'local')
+try:
+    config_pos = sys.argv.index('--app-config')
+    APP_CONFIG = sys.argv.pop(config_pos + 1)
+    sys.argv.pop(config_pos)
+except IndexError:
+    raise Exception("You must supply a configuration with --app-config")
+except ValueError:
+    APP_CONFIG = os.environ.get('APP_CONFIG', 'local')
+
 print "MANAGE APP_CONFIG:%s" % APP_CONFIG
 
 settings_module = 'config.%s.settings' % APP_CONFIG
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
-def dotenv_load(dotenv_file):
-    """
-    Loads a given .env file and merges it's contents with os.environ
-    """
-    try:
-        dotenv = imp.load_source('dotenv', dotenv_file)
-        dotenv = {attr:getattr(dotenv, attr) for attr in dir(dotenv) if not attr.startswith('_')}
-        os.environ.update(dotenv)
-
-        print "Loading .env vars from: %s" % dotenv_file
-        pprint.pprint(dotenv)
-    except IOError:
-        print "%s file not found. skipping." % dotenv_file
-
-# Merge in .env files to os.environ. We are kind of cheating by having .env files that
-# have the same syntax as a python module where every attribute is a string
-
-dotenv_load('config/common/.env')
-dotenv_load('config/%s/.env' % APP_CONFIG)
+# Merge in .env files to os.environ.
+utils.dotenv_load('config/common/.env')
+utils.dotenv_load('config/%s/.env' % APP_CONFIG)
 
 # Look for red flags
 check_sanity()
