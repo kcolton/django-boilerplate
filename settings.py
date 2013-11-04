654eecc557c6611ffea5016574be0e5df432c3de
Ken Colton <kcolton@gmail.com>
2013-11-04 10:47:29 -0500

    "fixed" main settings file and added spacing LESS

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 import importlib
 import os
 
-from config.common.settings import *
-
 config_settings = 'config.%s.settings' % os.environ['APP_CONFIG']
-importlib.import_module(config_settings)
+print "importing settings: %s" % config_settings
+
+# todo - gross gross gross gross
+module = __import__(config_settings, globals(), locals(), ['*'])
+for k in dir(module):
+    locals()[k] = getattr(module, k)
