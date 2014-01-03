1987f13f59e6721db13aa3166b3856e1a6a89739
Ken Colton <kcolton@gmail.com>
2014-01-02 22:21:31 -0500

    ghetto setup files

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='django-boilerplate',
+    version='0.0.1',
+    description='Boilerplate code I keep including in all Django installs.',
+    author='Ken Colton',
+    author_email='kcolton@gmail.com',
+    url='https://github.com/kcolton/django-boilerplate',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
