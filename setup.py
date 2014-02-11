4f7c2279b960f2fd4b5e4ca749f009b1ddec9346
Ken Colton <kcolton@gmail.com>
2014-02-11 13:04:24 -0500

    r++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-boilerplate',
-    version='0.0.7',
+    version='0.0.8',
     description='Boilerplate code I keep including in all Django installs.',
     author='Ken Colton',
     author_email='kcolton@gmail.com',
