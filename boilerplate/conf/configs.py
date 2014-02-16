3616a4ada702123666793ab3d394a7b42925cf78
Ken Colton <kcolton@gmail.com>
2014-02-16 15:23:45 -0500

    Move commonly edited settings into settings template

--- boilerplate/conf/configs.py
+++ boilerplate/conf/configs.py
@@ -1,24 +1,10 @@
-import os
-
 from boto.s3.connection import OrdinaryCallingFormat
 from configurations import Configuration, values
 from jinja2 import Undefined, DebugUndefined, StrictUndefined
 from boilerplate import assets
 
-import logging
-
-ENV_LOCAL = 'local'
-ENV_DEV = 'dev'
-ENV_STAGE = 'stage'
-ENV_PROD = 'prod'
-
-STORAGE_LOCAL = 'local'
-STORAGE_DEV = 'dev'
-STORAGE_PROD = 'prod'
-
 
 class Base(Configuration):
-    ENV = ENV_LOCAL
     TEMPLATE_DEBUG = DEBUG = True
 
     # Overwrite in app settings
@@ -26,43 +12,9 @@ class Base(Configuration):
     APP_NAME = 'djbp'
     TITLE = 'DJBP'
 
-    PROJECT_ROOT = None
-
-    WSGI_APPLICATION = None
-    ROOT_URLCONF = None
-    AUTH_USER_MODEL = None
-
     GOOGLE_TAG_MANAGER_CONTAINER = values.Value(None, environ_prefix=None)
 
     MANAGERS = ADMINS = ()
-
-    INSTALLED_APPS = (
-        'hijax',
-        'boilerplate',
-
-        'coffin',
-        'widget_tweaks',
-        'pipeline',
-
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
-
-        'suit',
-        'django.contrib.admin',
-    )
-
-    MIDDLEWARE_CLASSES = (
-        'django.middleware.common.CommonMiddleware',
-
-        'hijax.plugins.Middleware',
-
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-    )
 
     TEMPLATE_CONTEXT_PROCESSORS = Configuration.TEMPLATE_CONTEXT_PROCESSORS + (
         'boilerplate.context_processors.context_processor',
@@ -86,67 +38,8 @@ class Base(Configuration):
         'cache_size': 1000,
     }
 
-    # Logging
-
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'simple': {
-                'format': '%(asctime)s: %(name)s %(levelname)s - %(message)s'
-            }
-        },
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse',
-            },
-            'require_debug_true': {
-                '()': 'django.utils.log.RequireDebugTrue',
-            },
-        },
-        'handlers': {
-            'debug_console': {
-                'class': 'logging.StreamHandler',
-                'filters': ['require_debug_true'],
-                'formatter': 'simple'
-            },
-            'console': {
-                'class': 'logging.StreamHandler',
-                'filters': ['require_debug_false'],
-                'level': logging.WARNING,
-                'formatter': 'simple'
-            },
-            'null': {
-                'class': 'django.utils.log.NullHandler',
-            }
-        },
-        'loggers': {
-            'django': {
-                'level': logging.NOTSET,
-                'handlers': [],
-                'propagate': True
-            },
-            'django.request': {
-                'level': logging.NOTSET,
-                'handlers': [],
-                'propagate': True
-            },
-            'py.warnings': {
-                'level': logging.NOTSET,
-                'handlers': [],
-                'propagate': True
-            },
-        },
-        'root': {
-            'level': logging.NOTSET,
-            'handlers': ['debug_console', 'console']
-        }
-    }
-
     # Localization
 
-    TIME_ZONE = 'America/New_York'
-    LANGUAGE_CODE = 'en-us'
     USE_I18N = True
     USE_L10N = True
     USE_TZ = True
@@ -155,7 +48,6 @@ class Base(Configuration):
 
     # Override in environment specific settings or .env file
     SECRET_KEY = values.Value(environ_prefix=None)
-
     ALLOWED_HOSTS = ['*']
 
     # Statics
@@ -191,21 +83,9 @@ class Base(Configuration):
     LODASH = True
     CDN_LIBRARIES = True
 
-
     JINJA2_EXTENSIONS = [
         'pipeline.jinja2.ext.PipelineExtension'
     ]
-
-    SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-
-    @classmethod
-    def setup(cls):
-        cls.STATIC_ROOT = os.path.join(cls.PROJECT_ROOT, 'collectedstatic')
-        cls.MEDIA_ROOT = os.path.join(cls.PROJECT_ROOT, 'media')
-        cls.DATABASES = values.DatabaseURLValue('sqlite:///' + os.path.join(cls.PROJECT_ROOT, 'data/db.sqlite'))
-
-        super(Base, cls).setup()
-
 
     @classmethod
     def post_setup(cls):
--- boilerplate/conf/project_template/settings.py
+++ boilerplate/conf/project_template/settings.py
@@ -1,9 +1,20 @@
 import os
-import pymysql
+import logging
 
+import pymysql
+from configurations import values
 from boilerplate.conf import configs
 
 pymysql.install_as_MySQLdb()
+
+ENV_LOCAL = 'local'
+ENV_DEV = 'dev'
+ENV_STAGE = 'stage'
+ENV_PROD = 'prod'
+
+STORAGE_LOCAL = 'local'
+STORAGE_DEV = 'dev'
+STORAGE_PROD = 'prod'
 
 
 class AppBase(configs.Base):
@@ -21,15 +32,101 @@ class AppBase(configs.Base):
     TIME_ZONE = 'America/New_York'
     LANGUAGE_CODE = 'en-us'
 
-    @classmethod
-    def setup(cls):
-        super(AppBase, cls).setup()
-        cls.INSTALLED_APPS = ('{{ project_name }}', ) + cls.INSTALLED_APPS
+    INSTALLED_APPS = (
+        '{{ project_name }}',
+
+        'hijax',
+        'boilerplate',
+
+        'coffin',
+        'widget_tweaks',
+        'pipeline',
+
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+
+        'suit',
+        'django.contrib.admin',
+    )
+
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.common.CommonMiddleware',
+
+        'hijax.plugins.Middleware',
+
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    )
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'collectedstatic')
+    MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+    DATABASES = values.DatabaseURLValue('sqlite:///' + os.path.join(PROJECT_ROOT, 'data/db.sqlite'))
+
+    SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {
+                'format': '%(asctime)s: %(name)s %(levelname)s - %(message)s'
+            }
+        },
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse',
+            },
+            'require_debug_true': {
+                '()': 'django.utils.log.RequireDebugTrue',
+            },
+        },
+        'handlers': {
+            'debug_console': {
+                'class': 'logging.StreamHandler',
+                'filters': ['require_debug_true'],
+                'formatter': 'simple'
+            },
+            'console': {
+                'class': 'logging.StreamHandler',
+                'filters': ['require_debug_false'],
+                'level': logging.WARNING,
+                'formatter': 'simple'
+            },
+            'null': {
+                'class': 'django.utils.log.NullHandler',
+            }
+        },
+        'loggers': {
+            'django': {
+                'level': logging.NOTSET,
+                'handlers': [],
+                'propagate': True
+            },
+            'django.request': {
+                'level': logging.NOTSET,
+                'handlers': [],
+                'propagate': True
+            },
+            'py.warnings': {
+                'level': logging.NOTSET,
+                'handlers': [],
+                'propagate': True
+            },
+        },
+        'root': {
+            'level': logging.NOTSET,
+            'handlers': ['debug_console', 'console']
+        }
+    }
 
 
 class Local(AppBase):
-    ENV = configs.ENV_LOCAL
-    STORAGE = configs.STORAGE_LOCAL
+    ENV = ENV_LOCAL
+    STORAGE = STORAGE_LOCAL
 
     DEBUG = TEMPLATE_DEBUG = True
 
@@ -43,19 +140,19 @@ class LocalNoDebug(Local):
 
 
 class Dev(configs.S3Assets, configs.CompressAssets, AppBase):
-    ENV = configs.ENV_DEV
-    STORAGE = configs.STORAGE_DEV
+    ENV = ENV_DEV
+    STORAGE = STORAGE_DEV
 
     DEBUG = TEMPLATE_DEBUG = True
 
 
 class Prod(configs.S3Assets, configs.CompressAssets, AppBase):
-    ENV = configs.ENV_PROD
-    STORAGE = configs.STORAGE_PROD
+    ENV = ENV_PROD
+    STORAGE = STORAGE_PROD
 
     DEBUG = TEMPLATE_DEBUG = False
     # add jinja2 options
 
 
 class Stage(Prod):
-    ENV = configs.ENV_STAGE
+    ENV = ENV_STAGE
